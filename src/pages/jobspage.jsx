91ce3c761c11be44595f3e8609184639529cd2f5
umer-1999 <umarawan4444@gmail.com>
2024-08-23 13:32:44 -0700

    First Commit

---
+++ src/pages/jobspage.jsx
@@ -0,0 +1,13 @@
+import JobListings from '../components/JobListings';
+import React from 'react'
+
+
+const jobspage = () => {
+  return (
+  <section className='bg-blue-50 px-4 py-6'>
+    <JobListings />
+  </section>
+  )
+}
+
+export default jobspage
